f2da8f131edc3d649d9bf0a2a7d626c7dcc42e02
Kim (she/hers) windows <kimberlym4488@comcast.net>
2022-04-03 15:58:55 -0700

    Further styling updates and videos

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -2,49 +2,65 @@ import React from "react";
 // import profilePic from
 
 const styles = {
-  aboutMe: {
-    backgroundColor: "#009ecc",
+  text: {
+    backgroundColor: "rgb(0, 165, 240,.8)",
     color: "white",
   },
 
-  text: {
-    backgroundColor: "transparent",
-    color: "black",
-  },
-
   profilePic: {
-    height: "100px",
-    width: "auto",
+    width: "70%",
+    height: "auto",
+    margin: "2rem",
+  },
+  cardBody: {
+    background: "rgb(241, 244, 245)",
+    background:
+      "linear-gradient(95deg, rgba(241, 244, 245,.2) 50%, rgba(255,255,255,1) 50%)",
+  },
+  span: {
+    fontWeight: "bold",
+    color: "#77949E",
   },
 };
 
 function MainBio() {
   return (
     <>
-      <section className="Website-Frame aboutMe">
-        <div className="card text-center" style={styles.text}>
-          <div className="card-header" id="aboutMe">
-            About me
-          </div>
-          <div className="card-body">
-            <img
-              src={`${process.env.PUBLIC_URL}/Kim-profile-pic.jpg`}
-              className="card-img-top profilePic"
-              style={styles.profilePic}
-              alt="myPic"
-            ></img>
-            <h5 className="card-title" style={styles.aboutMe}>
-              I love to create
-            </h5>
-            <p className="card-text">
+      <section className="Website-Frame aboutMe ">
+        <div style={styles.text}>
+          <div className="text-center py-4">About me</div>
+        </div>
+        <div
+            className="card-body d-flex row justify-content-center align-items-center py-5"
+            style={styles.cardBody}
+          >
+            <p className="card-text col-sm-12 col-md-12 col-lg-6 col-xl-6 lh-lg px-4">
               I'm a creative problem solver with a love for technology. I love
               learning new things and overcoming a challenge. My experience with
               various software programs and working on a team has helped me to
               become a well-rounded, experienced, and reliable teammate.
             </p>
-          </div>
+
+            <div className="d-flex col-sm-12 col-md-12 col-lg-6 col-xl-6 lh-lg justify-content-center align-items-center py-4">
+              <div className="row justify-content-center align-items-center">
+                <div className="text-center col-6 align-items-center display-5 ">
+                  <h4>I</h4>
+                  <h4>love</h4>
+                  <h4>to</h4>
+                  <h4 style={styles.span}>create</h4>
+                </div>
+                  <div className="col-6 ">
+                    <img
+                      src={`${process.env.PUBLIC_URL}/Kim-profile-pic.jpg`}
+                      className="card-img-top profilePic"
+                      style={styles.profilePic}
+                      alt="myPic"
+                    ></img>
+                  </div>
+              </div>
+            </div>
         </div>
-      </section>
+        </section>
     </>
   );
 }
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,11 +1,99 @@
 import React from "react";
+import portfolioItems from "../data/portfolio.json";
 
-function AboutMe() {
+const styles = {
+  text: {
+    backgroundColor: 'rgb(241, 244, 245,1)',
+    color: "black",
+  },
+
+  frame: {
+    position: "relative",
+    paddingBottom: "56.25%",
+    height: 0,
+  },
+
+  iFrame: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+  },
+
+  cardHolder: {
+    background: "rgb(0, 165, 240)",
+    background:
+    "linear-gradient(75deg, rgba( 0, 165, 240,.03) 50%, rgba(255,255,255,1) 50%)",
+  },
+
+  span: {
+    fontWeight: "bold",
+    color: "rgba(0, 165, 240,1)",
+  },
+};
+
+const Projects = () => {
   return (
     <>
- 
+      <div style={styles.text}>
+        <div className=" text-center py-4">Projects</div>
+      </div>
+
+      <div
+        className="row justify-content-space-between align-items-center "
+        style={styles.cardHolder}
+      >
+        <div className="text-center py-4 gy-4 display-5">
+          <h4>My</h4>
+          <h4>passion</h4>
+          <h4 style={styles.span}>projects</h4>
+        </div>
+        <div className="row">
+          {portfolioItems.map(({ id, title, video, href, repo }) => (
+            <>
+              <div className="col-md-6 col-xl-6 col-xxl-6 text-center">
+                <p>{title}</p>
+                <div className="d-grid d-md-block gap-2">
+                  <button className="btn btn-primary" type="button">
+                    <a href={href} target="_blank" rel="noreferrer">
+                      Experience
+                    </a>
+                  </button>
+                  <button className="btn btn-outline-primary" type="button">
+                    <a href={repo} target="_blank">
+                      Repo
+                    </a>
+                  </button>
+                </div>
+              </div>
+
+              <div className="col-md-6 col-xl-6 col-xxl-6  text-center ">
+                <div className="card card-body shadow-sm">
+                  <div
+                    className="frame"
+                    key={id}
+                    style={styles.frame}
+                    onClick={() => {}}
+                  >
+                    <iframe
+                      title={title}
+                      src={video}
+                      frameborder="0"
+                      webkitallowfullscreen
+                      mozallowfullscreen
+                      allowfullscreen
+                      style={styles.iFrame}
+                    ></iframe>
+                  </div>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
     </>
   );
-}
+};
 
-export default AboutMe;
+export default Projects;
